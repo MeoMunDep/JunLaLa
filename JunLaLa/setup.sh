7785eaeeec83eb2d292c0594856f186ac7485446
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-03 03:08:41 +0700

    added upload image for AI feature

--- JunLaLa/setup.sh
+++ JunLaLa/setup.sh
@@ -30,16 +30,13 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "delayEachAccount": [1, 81],
+  "doTasks": true,
+  "referralCode": "2ngFMpB2",
+  "useLocalIamge": true
 }
 EOL
 }
@@ -55,7 +52,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    JunLaLa BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +112,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
